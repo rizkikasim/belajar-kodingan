f5b5907fdda4c14b5a7ad82e31af0a611b8d3da1
rizkikasim <rizkikasim224@gmail.com>
2023-01-26 22:56:09 +0700

    peubahan ke output aplikasi versi 2

--- hellowold.go
+++ hellowold.go
@@ -2,15 +2,8 @@ package main
 
 import (
 	"fmt"
-	"strings"
 )
 
 func main() {
-	var names = []string{"John", "Wick"}
-	printMessage("halo", names)
-}
-
-func printMessage(message string, arr []string) {
-	var nameString = strings.Join(arr, " ")
-	fmt.Println(message, nameString)
+	fmt.Println("aplikasi veris 2")
 }
